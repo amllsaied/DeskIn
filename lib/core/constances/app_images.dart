af8c00adfaac73df78cee2d112e5fec9a111ab98
Aml <amlsaied2004@gmail.com>
2026-09-17 21:55:00 +0300

    Add constants for app images and hardcoded strings
    - Created AppImages class to store image asset paths.
    - Added app logo and deskin image paths.
    - Created AppStrings class for hardcoded strings.
    - Defined app name as 'DeskIn'.

---
+++ lib/core/constances/app_images.dart
@@ -0,0 +1,4 @@
+class AppImages {
+  static const String appLogo = 'assets/icons/logo_icon.png';
+  static const String deskinImage = 'assets/images/deskin_image.png';
+}
---
+++ lib/core/constances/hard_strings.dart
@@ -0,0 +1,8 @@
+class AppStrings {
+  static const String appName = 'DeskIn';
+  //Splash Screen 
+  static const String find = 'Find';
+  static const String book = 'Book';
+  static const String work = 'Work';
+
+}
